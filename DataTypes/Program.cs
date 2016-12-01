dfc7d942456981afc6842eaeeacc5e0864242604
Abraham Araujo Moreno <alex_91y@hotmail.com>
2016-12-01 00:25:50 -0600

    Add files via upload

---
+++ DataTypes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Tipos de datos*/
+            bool sentencia = true;
+            int entero = 0;
+            decimal dec = 12.1222m;
+            float numFloat = 12.2323f;
+
+            Console.WriteLine("Min = {0}", int.MinValue);
+            Console.WriteLine("Max = {0}", int.MaxValue);
+            Console.ReadKey();
+        }
+    }
+}
